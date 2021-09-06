aca7d5515d19d891d01a460bb068a04fc80c37fc
Crowdbotics <team@crowdbotics.com>
2021-09-06 06:40:45 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/myanmar_net_ap_30344/MainActivity.java
@@ -0,0 +1,15 @@
+package com.myanmar_net_ap_30344;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "myanmar_net_ap_30344";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "myanmar_net_ap_30344Identifier",
+  url: "https://myanmar_net_ap_30344Identifier.botics.co",
+  api: "https://myanmar_net_ap_30344Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
